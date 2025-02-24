410a6ea3f198496e98cfc5414f9f6d51a24a2045
Karlo10ssj <karlo.pachecano10@ciencias.unam.mx>
2025-02-23 23:20:25 -0600

    Update Cliente.py

--- Teoria/ClasesYMetodos/Cliente.py
+++ Teoria/ClasesYMetodos/Cliente.py
@@ -3,7 +3,7 @@ class Cliente:
         self.nombre = nombre
         self.direccion = direccion
         self.edad = edad
-    def ImprimirDetalle(self): 
+    def imprimirDetalle(self): 
         print("Nombre: ", self.nombre,"Direccion", self.direccion, Edad: ", self.edad)
 cliente1= Cliemte("Carlos", "calle Epuris 123", 35)
-cliente1.ImprimirDetalles()
+cliente1.imprimirDetalles()
