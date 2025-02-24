d71b4e19be9b84cf0c38f6fc9d9cfd7017ac40dd
Karlo10ssj <karlo.pachecano10@ciencias.unam.mx>
2025-02-23 23:19:27 -0600

    Create Main.py

---
+++ Teoria/ClasesYMetodos/Main.py
@@ -0,0 +1,15 @@
+from Cliente import *
+from Cuenta import *
+
+class Main: 
+    pass
+class mensajeBienvenida(): 
+    def _init_():
+        pass
+    def darBienvenida(): 
+        print("Hola")
+mensajeBienvenida.darBienvenida()
+
+Juan = Cliente("Maria", "Iztapalapa 11", 18)
+imprimirDetalles(Juan)
+cuenta1 = Cuenta(250,"debito", "Juan")
