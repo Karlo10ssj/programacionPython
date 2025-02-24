dad5ecdfbef3233b1801bca422d62b4f875256ec
Karlo10ssj <karlo.pachecano10@ciencias.unam.mx>
2025-02-23 23:09:40 -0600

    Create Cliente.py

---
+++ Teoria/ClasesYMetodos/Cliente.py
@@ -0,0 +1,9 @@
+class Cliente: 
+    def _init_(self, nombre, direccion, edad):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.edad = edad
+    def ImprimirDetalle(self): 
+        print("Nombre: ", self.nombre,"Direccion", self.direccion, Edad: ", self.edad)
+cliente1= Cliemte("Carlos", "calle Epuris 123", 35)
+cliente1.ImprimirDetalles()
