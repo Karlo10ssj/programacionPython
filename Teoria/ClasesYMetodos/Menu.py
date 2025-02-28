f699e887a16867e54cc559351c7cc000f6606699
Karlo10ssj <karlo.pachecano10@ciencias.unam.mx>
2025-02-28 15:34:11 -0600

    Update and rename Menu to Menu.py

---
+++ Teoria/ClasesYMetodos/Menu.py
@@ -0,0 +1,9 @@
+#Author: Antonio Karlo Hernandez Pachecano
+class Menu: 
+    def __init__(self):
+        self.mensaje = mensaje
+    def mostrar (self):
+        print(self.mensaje)
+menu= Menu("Bienvenido al cajero automatico")
+menu.mostrar()
+    pass
