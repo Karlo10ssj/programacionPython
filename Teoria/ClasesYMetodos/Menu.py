0af6edef3e756d9fec4c623c0fbbd419be903d3e
Karlo10ssj <karlo.pachecano10@ciencias.unam.mx>
2025-03-12 22:39:19 -0600

    Update Menu.py

--- Teoria/ClasesYMetodos/Menu.py
+++ Teoria/ClasesYMetodos/Menu.py
@@ -1,9 +1,32 @@
 #Author: Antonio Karlo Hernandez Pachecano
-class Menu: 
+# Menu.py
+
+class Menu:
     def __init__(self):
-        self.mensaje = mensaje
-    def mostrar (self):
+        self.mensaje = "Bienvenido al cajero automático"
+
+    def mostrar(self):
         print(self.mensaje)
-menu= Menu("Bienvenido al cajero automatico")
-menu.mostrar()
-    pass
+        while True:
+            print("1. Retirar")
+            print("2. Depositar")
+            print("3. Salir")
+            opcion = input("Seleccione una opción: ")
+            if opcion == "1":
+                self.OpcionRetirar()
+            elif opcion == "2":
+                self.OpcionDepositar()
+            elif opcion == "3":
+                self.OpcionSalir()
+                break
+            else:
+                print("Opción no válida. Por favor, seleccione 1, 2 o 3.")
+
+    def OpcionRetirar(self):
+        print("Desde la opción Retirar")
+
+    def OpcionDepositar(self):
+        print("Desde la opción Depositar")
+
+    def OpcionSalir(self):
+        print("Saliendo del cajero automático")
